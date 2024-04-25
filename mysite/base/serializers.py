f1efc07ce568f37dd006e51747d29abed8597e3e
kimq3 <134682597+kimq3@users.noreply.github.com>
2024-04-25 21:37:55 +0900

    Revert "master에서 branch 신규 생성에 의한 첫 합치기"

--- mysite/base/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from base.models import Example
-
-class ExampleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Example
-        fields = ['id', 'created', 'title', 'price', 'photo', 'stock']
--- mysite/base/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from base import views
-
-urlpatterns = [
-    path('example/', views.base_list),
-    path('getimage/<str:filename>/',views.get_image)
-]
