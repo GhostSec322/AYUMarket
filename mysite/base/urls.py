0b5c2182379ff770c9ba4348bfcef221b8fd1c74
kimq3 <kgs6597@naver.com>
2024-04-27 17:41:07 +0900

    query params로 detail 페이지 넘어갈시 해당 상품 정보 전달하기 기능 구현

--- mysite/base/urls.py
+++ mysite/base/urls.py
@@ -3,5 +3,7 @@ from base import views
 
 urlpatterns = [
     path('example/', views.base_list),
-    path('getimage/<str:filename>/',views.get_image)
+    path('getimage/<str:filename>/',views.get_image),
+    #path('detail/<int:pk>/', views.product_detail),
+    path('detail/',views.product_detail)
 ]
--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,12 +1,12 @@
+from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Example
 from base.serializers import ExampleSerializer
 import os
-from django.http import HttpResponse
 
-@api_view(['GET','POST'])
+@api_view(['GET','POST']) #나열할 상품 전체 가져오기
 def base_list(request, format=None):
     if request.method == 'GET':
         baseData= Example.objects.all()
@@ -20,7 +20,7 @@ def base_list(request, format=None):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+@api_view(['GET'])  #서버내 저장된 이미지 가져오기
 def get_image(request, filename):
     current_directory=os.getcwd() #현재 디렉토리 추출
     file_path = os.path.join('base\photos', filename) #url에서 입력된 파일 이름으로 서버내 파일로 진입
@@ -34,3 +34,30 @@ def get_image(request, filename):
         return HttpResponse(photo_data, content_type='image/png')
     except FileNotFoundError:
         return HttpResponse(status=404)
+
+'''   
+@api_view(['GET']) #특정 상품 detail 가져오기 쿼리 파라미터 안쓴 ver
+def product_detail(request,pk):
+    try:
+        product = Example.objects.get(pk=pk) 
+    except Example.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer=ExampleSerializer(product)
+    return Response(serializer.data)
+'''
+
+@api_view(['GET'])
+def product_detail(request):
+    product_id=request.query_params.get('product_id',None)
+    if product_id is None:
+        return Response({'에러 에러 id 어디감?'},status=status.HTTP_400_BAD_REQUEST)
+    
+    try:
+        product=Example.objects.get(pk=product_id)
+    
+    except Example.DoesNotExist:
+        return Response({'에러 id 번호가 없는디?'}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer=ExampleSerializer(product)
+    return Response(serializer.data)
