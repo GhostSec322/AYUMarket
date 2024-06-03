0de8fa8bb30236dc1afb5f300717021601253db9
GhostSec322 <GhostSec322@gmail.com>
2024-06-03 20:02:11 +0900

    1차 충돌 해결

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Cart, Category, Item, Qna,Order,RefundRequest, UserLogin, Order
-from .serializers import CartGetSerializer, CartSerializer, ItemSerializer, LoginSerializer, QnaSerializer,OrderSerializer, RegisterSerializer
+from .serializers import CartGetSerializer, CartSerializer, ItemSerializer, LoginSerializer, QnaSerializer,OrderSerializer, RegisterSerializer ,CategorySerializer
 from base.models import Example
 from base.serializers import ExampleSerializer
 import os
